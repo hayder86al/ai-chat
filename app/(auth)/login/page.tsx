21f1da6164dced0c965a59a66d1031a2f52bf8aa
Hayder Alraeai <alraeai.hayder@gmail.com>
2024-10-29 20:54:54 +0100

    Add @ai-sdk/anthropic dependency and refactor code for consistency

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,39 +1,39 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useActionState, useEffect, useState } from "react";
-import { toast } from "sonner";
+// import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useActionState, useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
-import { AuthForm } from "@/components/custom/auth-form";
-import { SubmitButton } from "@/components/custom/submit-button";
+import { AuthForm } from '@/components/custom/auth-form';
+import { SubmitButton } from '@/components/custom/submit-button';
 
-import { login, LoginActionState } from "../actions";
+import { login, LoginActionState } from '../actions';
 
 export default function Page() {
   const router = useRouter();
 
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
 
   const [state, formAction] = useActionState<LoginActionState, FormData>(
     login,
     {
-      status: "idle",
-    },
+      status: 'idle',
+    }
   );
 
   useEffect(() => {
-    if (state.status === "failed") {
-      toast.error("Invalid credentials!");
-    } else if (state.status === "invalid_data") {
-      toast.error("Failed validating your submission!");
-    } else if (state.status === "success") {
+    if (state.status === 'failed') {
+      toast.error('Invalid credentials!');
+    } else if (state.status === 'invalid_data') {
+      toast.error('Failed validating your submission!');
+    } else if (state.status === 'success') {
       router.refresh();
     }
   }, [state.status, router]);
 
   const handleSubmit = (formData: FormData) => {
-    setEmail(formData.get("email") as string);
+    setEmail(formData.get('email') as string);
     formAction(formData);
   };
 
@@ -48,7 +48,7 @@ export default function Page() {
         </div>
         <AuthForm action={handleSubmit} defaultEmail={email}>
           <SubmitButton>Sign in</SubmitButton>
-          <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
+          {/* <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
             {"Don't have an account? "}
             <Link
               href="/register"
@@ -57,7 +57,7 @@ export default function Page() {
               Sign up
             </Link>
             {" for free."}
-          </p>
+          </p> */}
         </AuthForm>
       </div>
     </div>
